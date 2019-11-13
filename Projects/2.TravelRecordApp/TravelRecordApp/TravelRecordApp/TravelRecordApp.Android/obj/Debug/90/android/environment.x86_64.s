7444d4e52bd7ab4366fa05f62ad837d2e62c763b
Glareone <enlarius@gmail.com>
2019-11-13 19:39:48 +0300

    fix

--- Projects/2.TravelRecordApp/TravelRecordApp/TravelRecordApp/TravelRecordApp.Android/obj/Debug/90/android/environment.x86_64.s
+++ Projects/2.TravelRecordApp/TravelRecordApp/TravelRecordApp/TravelRecordApp.Android/obj/Debug/90/android/environment.x86_64.s
@@ -62,7 +62,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"4e5d4864-5e18-4814-b6d3-0da7f565b3ed"
+	.asciz	"06c30c28-d8eb-410a-a23f-29da81cd83db"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
