96be47dde6093a88253365a619d8094cbe5d3e83
Glareone <enlarius@gmail.com>
2019-11-18 15:52:38 +0300

    init references and clientid and secret for foursquare api.

---
+++ Projects/2.TravelRecordApp/TravelRecordApp/TravelRecordApp/TravelRecordApp/Helpers/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelRecordApp.Helpers
+{
+    public class Constants
+    {
+        public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
+
+        public const string CLIENT_ID = "B2SSL2R43XTK2212EPWKIDSQXL5L1BXPAJZIZPQSNKRAC0E3";
+
+        public const string CLIENT_SECRET = "IUW0EWTBXUYEP1LRHLS1VWZR4XKXCRDVXP0MUZM1TG0TADOQ";
+    }
+}
