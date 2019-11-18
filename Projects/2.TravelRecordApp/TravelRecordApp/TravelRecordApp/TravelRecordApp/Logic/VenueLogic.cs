ff18505ca547cdec2703e4e97efc32e8c7fbdb4c
Glareone <enlarius@gmail.com>
2019-11-18 18:09:00 +0300

    fixed issues with internet connection, map and foursquare

---
+++ Projects/2.TravelRecordApp/TravelRecordApp/TravelRecordApp/TravelRecordApp/Logic/VenueLogic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TravelRecordApp.Model;
+
+namespace TravelRecordApp.Logic
+{
+    public class VenueLogic
+    {
+        public static async Task<List<Venue>> GetVenues(double latitude, double longitude)
+        {
+            var venues = new List<Venue>();
+
+            var url = Venue.GenerateURL(latitude, longitude);
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                var json = await response.Content.ReadAsStringAsync();
+            }
+
+            return venues;
+        }
+    }
+}
