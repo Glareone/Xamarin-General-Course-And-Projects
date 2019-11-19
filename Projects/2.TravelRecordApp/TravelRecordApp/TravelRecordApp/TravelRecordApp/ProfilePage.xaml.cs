2e93c992ef7386a7b1699dd95873a8116109560e
Glareone <enlarius@gmail.com>
2019-11-20 00:33:30 +0300

    linq in ProfilePage. Finished Section 10

--- Projects/2.TravelRecordApp/TravelRecordApp/TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
+++ Projects/2.TravelRecordApp/TravelRecordApp/TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using System.Linq;
+using SQLite;
+using TravelRecordApp.Model;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace TravelRecordApp
@@ -9,6 +12,30 @@ namespace TravelRecordApp
         public ProfilePage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using (var connection = new SQLiteConnection(App.DatabaseLocation))
+            {
+                var postTable = connection.Table<Post>().ToList();
+
+                var postsCategories = (from p in postTable
+                        orderby p.CategoryId
+                        select p.CategoryName).Distinct().ToList();
+
+                var categoriesWithCount = postsCategories.Select(category => new
+                {
+                    categoryName = category,
+                    categoryCount = postTable.Count(post => post.CategoryName == category)
+                }).ToDictionary(k => k.categoryName, v => v.categoryCount);
+
+                categoriesListView.ItemsSource = categoriesWithCount;
+
+                postCountLabel.Text = postTable.Count.ToString();
+            }
         }
     }
 }
