2eb06552c33ad8510e066471d364dfd1ba6dd07a
Glareone <enlarius@gmail.com>
2019-11-26 21:42:21 +0300

    ImageUploader Initial commit + permissions + image selection

---
+++ Projects/3.CloudImages/ImageUploader/ImageUploader/ImageUploader/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+
+namespace ImageUploader
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void PictureSelectButton_OnClicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Error", "Not supported on your device", "Ok");
+                return;
+            }
+
+            // Make photo smaller
+            var mediaOptions = new PickMediaOptions
+            {
+                PhotoSize = PhotoSize.Medium
+            };
+
+            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
+
+            if (selectedImageFile == null)
+            {
+                await DisplayAlert("Warning", "Please select the image first", "Ok");
+                return;
+            }
+
+            SelectedImage.Source = ImageSource.FromStream(() => selectedImageFile.GetStream());
+        }
+    }
+}
