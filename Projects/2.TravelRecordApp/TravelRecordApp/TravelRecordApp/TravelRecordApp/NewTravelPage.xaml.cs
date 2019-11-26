01576444e460320506256a323b95b7728898f22f
Glareone <enlarius@gmail.com>
2019-11-26 19:39:58 +0300

    permissions example

--- Projects/2.TravelRecordApp/TravelRecordApp/TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
+++ Projects/2.TravelRecordApp/TravelRecordApp/TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Plugin.Geolocator;
+﻿using System;
+using Plugin.Geolocator;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 using TravelRecordApp.Model;
 using TravelRecordApp.ViewModel;
 using Xamarin.Forms;
@@ -22,13 +25,46 @@ namespace TravelRecordApp
         protected override async void OnAppearing()
         {
             base.OnAppearing();
+            try
+            {
+                // do we have permissions to get user's location. If not - request this access from user.
+                var status =
+                    await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Location);
 
-            var locator = CrossGeolocator.Current;
-            var position = await locator.GetPositionAsync();
+                if (status != PermissionStatus.Granted)
+                {
+                    if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Location))
+                    {
+                        await DisplayAlert("Need permission", "We will have to access to your location", "Ok");
+                    }
 
-            var venues = await Venue.GetVenues(position.Latitude, position.Longitude);
+                    var permissionRequestResults =
+                        await CrossPermissions.Current.RequestPermissionsAsync(Permission.Location);
 
-            venueListView.ItemsSource = venues;
+                    if (permissionRequestResults.ContainsKey(Permission.Location))
+                    {
+                        status = permissionRequestResults[Permission.Location];
+                    }
+                }
+
+                if (status == PermissionStatus.Granted)
+                {
+                    var locator = CrossGeolocator.Current;
+                    var position = await locator.GetPositionAsync();
+
+                    var venues = await Venue.GetVenues(position.Latitude, position.Longitude);
+                    venueListView.ItemsSource = venues;
+                }
+                else
+                {
+                    await DisplayAlert("No permission",
+                        "You didn't granted permission to access to your location, we cannot proceed", "Ok");
+                }
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("Oops", "Something wrong", "Ok");
+            }
         }
     }
 }
