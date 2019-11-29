d35871237110da1e32ee3e00286380648b28d2a9
Glareone <enlarius@gmail.com>
2019-11-30 01:04:17 +0300

    section-18 is finished

--- Projects/4.Native-DeliveriesApp/DeliveriesApp/DeliveriesApp/Model/AzureHelper.cs
+++ Projects/4.Native-DeliveriesApp/DeliveriesApp/DeliveriesApp/Model/AzureHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.WindowsAzure.MobileServices;
 
 namespace DeliveriesApp.Model
@@ -6,6 +7,5 @@ namespace DeliveriesApp.Model
     public class AzureHelper
     {
         public static MobileServiceClient MobileServiceClient = new MobileServiceClient("https://xamarindeliveriesappglareone.azurewebsites.net", new HttpClientHandler());
-
     }
 }
---
+++ Projects/4.Native-DeliveriesApp/DeliveriesApp/DeliveriesApp/Model/Delivery.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DeliveriesApp.Model
+{
+    public class Delivery
+    {
+        public string Id { get; set; }
+
+        public static async Task<IEnumerable<Delivery>> GetDeliveries(string id)
+        {
+            return await AzureHelper.MobileServiceClient.GetTable<Delivery>().Where(d => d.Id == id).ToListAsync();
+        }
+    }
+}
