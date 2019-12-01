693a661b8a9fc799ad9f4a327979bcff1507470d
Glareone <enlarius@gmail.com>
2019-12-02 00:55:15 +0300

    step 1

--- Projects/4.Native-DeliveriesApp/DeliveriesApp/DeliveriesApp/Model/Delivery.cs
+++ Projects/4.Native-DeliveriesApp/DeliveriesApp/DeliveriesApp/Model/Delivery.cs
@@ -22,15 +22,27 @@ namespace DeliveriesApp.Model
         // 2 = delivered
         public int Status { get; set; }
 
-        public static async Task<IEnumerable<Delivery>> GetDeliveries(string id)
+        public static async Task<IEnumerable<Delivery>> GetDeliveries()
         {
-            return await AzureHelper.MobileServiceClient.GetTable<Delivery>().Where(d => d.Id == id).ToListAsync();
+            var deliveries = await AzureHelper.MobileServiceClient.GetTable<Delivery>().Where(d => d.Status != 2).ToListAsync();
+            return deliveries;
+        }
+
+        public static async Task<IEnumerable<Delivery>> GetDelivered()
+        {
+            var deliveries = await AzureHelper.MobileServiceClient.GetTable<Delivery>().Where(d => d.Status == 2).ToListAsync();
+            return deliveries;
         }
 
         public static async Task<bool> InsertDelivery(Delivery delivery)
         {
             await AzureHelper.MobileServiceClient.GetTable<Delivery>().InsertAsync(delivery);
             return true;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Status}";
         }
     }
 }
