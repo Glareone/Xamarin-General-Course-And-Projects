a8be2a66e0c9c25981a2fcc4f1193b74bd3e8f9b
Glareone <enlarius@gmail.com>
2019-12-01 21:13:24 +0300

    Tabs + Menu. Fixed problem with Resource.Ids. Added icons

---
+++ Projects/4.Native-DeliveriesApp/DeliveriesApp/DeliveriesApp.Droid/NewDeliveryActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace DeliveriesApp.Droid
+{
+    [Activity(Label = "NewDeliveryActivity")]
+    public class NewDeliveryActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.NewDelivery);
+        }
+    }
+}
